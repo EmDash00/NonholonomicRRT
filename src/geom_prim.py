5502e61692bc2e9d58f02031f29d8e15db19d92b
Emmy Chow <emchow@iu.edu>
2021-12-12 17:44:25 -0500

    fixed primative generation

--- src/geom_prim.py
+++ src/geom_prim.py
@@ -6,47 +6,51 @@ from os import path
 
 
 L = 0.1
-v_max = 1
+v_max = 5
 T = 0.1  # Amount of time to simulate into the future
 
 N_phi = 21  # Number of turning angles to generate
-N_t = 100  # Resolution of Euler integration
-N_v = 100  # Number of velocities to integrate over
+N_t = 10  # Resolution of Euler integration
+N_v = 500  # Number of velocities to integrate over
 dt = T / N_t
 phi = np.linspace(-0.4 * np.pi, 0.4 * np.pi, N_phi)
 
 
 def generate_primatives():
-    primatives = np.zeros((N_t, N_v, N_phi, 3))
-    neg_primatives = np.zeros_like(primatives)
+    primatives = np.zeros((N_t * 2, N_v, N_phi, 3))
+    pos_prim = primatives[:N_t]
+    neg_prim = primatives[N_t:]
 
-    for i in range(1, N_t):
-        for j in range(N_v):
-            v = i * v_max / N_v
 
-            primatives[i, j, :, 0] = (
-                primatives[i - 1, j, :, 0] +
-                v * cos(phi) * cos(primatives[i, j - 1, :, 2] * 2 * pi) * dt
+    for i in range(N_v):
+        v = i * v_max / N_v
+
+        for j in range(1, N_t):
+            pos_prim[j, i, :, 0] = (
+                pos_prim[j - 1, i, :, 0] +
+                v * np.cos(phi) * np.cos(pos_prim[j - 1, i, :, 2] * 2 * pi) * dt
             )
 
-            primatives[i, j, :, 1] = (
-                primatives[i - 1, j, :, 1] +
-                v * cos(phi) * sin(primatives[i, j - 1, :, 2] * 2 * pi) * dt
+            pos_prim[j, i, :, 1] = (
+                pos_prim[j - 1, i, :, 1] +
+                v * np.cos(phi) * np.sin(pos_prim[j - 1, i, :, 2] * 2 * pi) * dt
             )
 
-            primatives[i, j, :, 2] = (
-                primatives[i - 1, j, :, 2] +
-                v / L * sin(phi) / (2 * pi) * dt
+            pos_prim[j, i, :, 2] = (
+                pos_prim[j - 1, i, :, 2] +
+                v / L * np.sin(phi) / (2 * pi) * dt
             )
+
+    pos_prim[..., 2] -= floor(pos_prim[..., 2])
 
     # Caveat, primatives moving in the opposite direction have
     # Orientation rotated by pi/2 (0.5 revs)
-    neg_primatives[..., :2] = -primatives[..., :2]
-    neg_primatives[..., 2] = primatives[..., 2] + 0.5
-    neg_primatives[..., 2] -= floor(neg_primatives[..., 2])
+    neg_prim[..., :2] = -pos_prim[..., :2]
+    neg_prim[..., 2] = pos_prim[..., 2] + 0.5
+    neg_prim[..., 2] -= floor(neg_prim[..., 2])
 
     # Negative v is just a sign change.
-    np.save("primatives.npy", np.concatenate((primatives, neg_primatives)))
+    np.save("primatives.npy", primatives)
 
 
 if __name__ == "__main__":
@@ -57,10 +61,10 @@ if not path.exists("./primatives.npy"):
 
 # (timestep, phi, x/y/theta)
 prims = np.load("primatives.npy")
-prims_pos = prims[0:N_t]
-prims_neg = prims[N_t:2 * N_t]
+prims_pos = prims[:N_t]
+prims_neg = prims[N_t:]
 
-primative_tree = [MTree(dist) for i in range(500)]
+primative_tree = [MTree(dist) for i in range(N_v)]
 
 for i in range(N_v):
     v = i * v_max / N_v
